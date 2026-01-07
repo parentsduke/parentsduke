72a759b7077786ce5f54861bf688b6f1f9ce1cb5
parentsduke <sws122003@gmail.com>
2026-01-07 14:58:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+<script>
+const overlay = document.getElementById('overlay');
+const overlayImg = document.getElementById('overlay-img');
 
+document.querySelectorAll('.thumb').forEach(img => {
+  img.onclick = () => {
+    overlay.style.display = 'flex';
+    overlayImg.src = img.src;
+  };
+});
+
+overlay.onclick = () => overlay.style.display = 'none';
+</script>
