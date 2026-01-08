a748f5e09d45182e3f11447f9b2799b07bb857ee
parentsduke <sws122003@gmail.com>
2026-01-07 22:59:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,38 @@
-
+/* ========== 图片单击放大 ========== */
 const thumbs = document.querySelectorAll('.thumb');
-const overlay = document.getElementById('overlay');
+const imgOverlay = document.getElementById('overlay');
 const overlayImg = document.getElementById('overlay-img');
 
 thumbs.forEach(img => {
   img.addEventListener('click', () => {
     overlayImg.src = img.src;
-    overlay.style.display = 'flex';
+    imgOverlay.style.display = 'flex';
   });
 });
 
-overlay.addEventListener('click', () => {
-  overlay.style.display = 'none';
+imgOverlay.addEventListener('click', () => {
+  imgOverlay.style.display = 'none';
 });
 
-const videos = document.querySelectorAll('.enlarge-video');
-const overlay = document.getElementById('video-overlay');
+
+/* ========== 视频单击放大（重点） ========== */
+const videoWrapper = document.querySelectorAll('.video-wrapper');
+const videoOverlay = document.getElementById('video-overlay');
 const overlayVideo = document.getElementById('overlay-video');
 
-// 点击视频 → 放大
-videos.forEach(video => {
-  video.addEventListener('click', () => {
-    overlayVideo.src = video.currentSrc;
-    overlay.style.display = 'flex';
-    overlayVideo.play();
-  });
-});
-
-// 点击黑色区域 → 关闭
-overlay.addEventListener('click', () => {
-  overlayVideo.pause();
-  overlayVideo.src = '';
-  overlay.style.display = 'none';
-});
-
-document.querySelectorAll('.video-wrapper').forEach(wrapper => {
+videoWrapper.forEach(wrapper => {
   const video = wrapper.querySelector('video');
 
   wrapper.addEventListener('click', () => {
     overlayVideo.src = video.currentSrc;
-    overlay.style.display = 'flex';
+    videoOverlay.style.display = 'flex';
     overlayVideo.play();
   });
+});
+
+// 点击黑色区域关闭
+videoOverlay.addEventListener('click', () => {
+  overlayVideo.pause();
+  overlayVideo.src = '';
+  videoOverlay.style.display = 'none';
 });
